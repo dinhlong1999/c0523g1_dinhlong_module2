3382876e28321417b8f9fea146d49afb0bb54646
dinhlong1999 <long.truong.vcr@gmail.com>
2023-07-04 11:47:59 +0700

    longnopbaitap

---
+++ src/exercise/Hello.java
@@ -0,0 +1,12 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please import your name: ");
+        String name = scanner.nextLine();
+        System.out.println("hello " + name);
+    }
+}
