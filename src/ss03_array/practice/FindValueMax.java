f56f83a9f4a92639c6667ccfbbc056e53e78663d
dinhlong1999 <long.truong.vcr@gmail.com>
2023-07-05 23:04:12 +0700

    longnopbaithuchanh

---
+++ src/ss03_array/practice/FindValueMax.java
@@ -0,0 +1,39 @@
+package ss03_array.practice;
+
+import java.util.Scanner;
+
+public class FindValueMax {
+    public static void main(String[] args) {
+        int size;
+        int arr[];
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Please import less 20 value");
+            size = scanner.nextInt();
+            if (size > 20) {
+                System.out.print("Invalid, must be less than 20: ");
+            }
+        } while (size > 20);
+        arr = new int[size];
+        int i = 0;
+        while (i < arr.length) {
+            System.out.print("Please import element" + (i + 1) + " : ");
+            arr[i] = scanner.nextInt();
+            i++;
+        }
+        System.out.print("Property list: ");
+        for (int j = 0; j < arr.length; j++) {
+            System.out.print(arr[j] + "\t");
+        }
+        int max = arr[0];
+        int index = 1;
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] > max) {
+                max = arr[j];
+                index = j + 1;
+            }
+        }
+        System.out.println("The largest property value in the list is " + max + " at " + index);
+
+    }
+}
