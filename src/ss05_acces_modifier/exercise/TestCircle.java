8d1d450ebedacdcd471c972a8ade0b88fb0546a8
dinhlong1999 <long.truong.vcr@gmail.com>
2023-07-09 23:15:26 +0700

    longnopbaitap

---
+++ src/ss05_acces_modifier/exercise/TestCircle.java
@@ -0,0 +1,11 @@
+package ss05_acces_modifier.exercise;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle testCircle = new Circle();
+        System.out.println("Radius of circle: " + testCircle.getRadius());
+        System.out.println("Area of circle:  "+ testCircle.getArea());
+    }
+
+
+}
