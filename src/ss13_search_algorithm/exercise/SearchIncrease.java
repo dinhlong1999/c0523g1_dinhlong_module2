9ee682bb3b8212ddca000cdc04bc0d68fdb19b0e
dinhlong1999 <long.truong.vcr@gmail.com>
2023-07-19 22:23:24 +0700

    longnopbaitap

---
+++ src/ss13_search_algorithm/exercise/SearchIncrease.java
@@ -0,0 +1,32 @@
+package ss13_search_algorithm.exercise;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SearchIncrease {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a text you want check: ");
+        String str = scanner.nextLine();
+        LinkedList<Character> max = new LinkedList<>();
+        LinkedList<Character> list = new LinkedList<>();
+        for (int i = 0; i < str.length(); i++) {
+            list.add(str.charAt(i));
+            for (int j = i + 1; j < str.length()  ; j++) {
+                    if (str.charAt(j) > list.getLast()){
+                        list.add(str.charAt(j));
+                    }
+            }
+                if(list.size() > max.size()){
+                    max.clear();
+                    max.addAll(list);
+                }
+            list.clear();
+        }
+        for (Character character : max) {
+            System.out.print(character);
+        }
+    }
+}
--- src/ss13_search_algorithm/practice/BinarySearchTree.java
+++ src/ss13_search_algorithm/practice/BinarySearchTree.java
@@ -25,7 +25,10 @@ public class BinarySearchTree {
         System.out.println(binarySearch(list,14));
         System.out.println(binarySearch(list,16));
         System.out.println(binarySearch(list,9));
+     
+
     }
+
 
 
 }
